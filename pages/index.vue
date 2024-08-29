0ea4c674b0ca8c58be0d3063fc144f8e4a59b43b
jayl <jayl@valueaddsofttech.com>
2024-08-29 15:00:50 +0530

    setup & config

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <div>
+    Welcome to Demo App
+    <p>
+      Click
+      <a
+        class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        href="/login"
+        >here</a
+      >
+      to Login
+    </p>
+  </div> -->
+  <div>
+    <h1 class="font-bold">Welcome to Demo App</h1>
+    <p>
+      Click
+      <a
+        class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        href="/login"
+        >here</a
+      >
+      to login
+    </p>
+  </div>
+</template>
